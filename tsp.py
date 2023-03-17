7a8c9cf232fe4bd72cfa0a8ed5961bed579dfc64
Ryan Twohey <rjtwoh01@gmail.com>
2023-03-16 21:25:43 -0400

    wireframe

--- tsp.py
+++ tsp.py
@@ -6,3 +6,33 @@ class TSP:
     def __init__(self) -> None:
         self.x = 0
         self.y = 0
+
+
+#Algorithms to use
+# 1. Brute Force (up to n=10 due to computer speed)
+# 2. Closest Edge
+# 3. DFA
+# 4. BFA
+
+def bruteForce():
+    return
+
+def closestEdge():
+    return
+
+def dfa():
+    return
+
+def bfa():
+    return
+
+def generatePoints(numberOfPoints):
+    pointList = list() #of type TSP
+    return pointList
+
+def runSimulation():
+    #we'll run all 4 algorithms
+    #collect their data
+    #run analysis
+    #print results
+    return
