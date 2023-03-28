cfb90807e6dd8af4eef08e9e51dbc3fbaaae0f1f
Ryan Twohey <rjtwoh01@gmail.com>
2023-03-27 21:29:32 -0400

    Got closest edge insertion working

--- tsp.py
+++ tsp.py
@@ -12,6 +12,19 @@ class TSP:
         if isinstance(other, TSP):
             return self.x == other.x and self.y == other.y
         return False
+
+class TSPEdge:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+        self.length = calculateDistanceBetweenPoints(p1, p2)
+
+    def distanceFromEdge(self, point):
+        distance1 = calculateDistanceBetweenPoints(self.p1, point)
+        distance2 = calculateDistanceBetweenPoints(self.p2, point)
+
+        return (distance1 + distance2 - self.length)
+
     
 class AlgorithmResult:
     def __init__(self, time, distance, combination) -> None:
@@ -39,8 +52,43 @@ def bruteForce(listOfPoints):
     bestTime = f"{time.time() - start_time}s"
     return AlgorithmResult(bestTime, bestDistance, printListOfPoints(bestCombination)) 
 
-def closestEdge():
-    return
+def closestEdge(listOfPoints):
+    startTime = time.time()
+    finalList = list() #TSP
+    edgesList = list() #TSPEdge
+
+    for i, point in enumerate(listOfPoints):
+        #first 3 nodes create their own 'triange'
+        if (i < 3):
+            j = i + 1
+            if j==3: j = 0
+            edgesList.append(TSPEdge(point, listOfPoints[j]))
+        else:
+            currentEdge = edgesList[0]
+            localDistance = edgesList[0].distanceFromEdge(point)
+
+            for j, edge in enumerate(edgesList):
+                tempDistance = edge.distanceFromEdge(point)
+                if tempDistance < localDistance:
+                    localDistance = tempDistance
+                    currentEdge = edge
+            index = edgesList.index(currentEdge)
+            edgesList.insert(index, TSPEdge(currentEdge.p1, point))
+            edgesList.insert(index+1, TSPEdge(point, currentEdge.p2))
+            edgesList.remove(currentEdge)
+    
+    for edge in edgesList:
+        finalList.append(edge.p1)
+    
+    endtime = time.time()
+    distance = 0
+    for i, point in enumerate(finalList):
+        if (i+1) in range(0, len(finalList)):
+            distance += calculateDistanceBetweenPoints(point, finalList[i+1])
+    
+
+    finalTime = f"{endtime - startTime}s"
+    return AlgorithmResult(finalTime, distance, printListOfPoints(finalList))
 
 def dfa():
     return
@@ -80,10 +128,24 @@ def printListOfPoints(listOfPoints):
 
 def runSimulation():
 
+    #we'll want to have the same list of cities for these algorithms to run through in the final version
+    #that way the data can be as accurately compared as possible
+    #but this is fine for verification
+
     numberOfCities = 3 
-    while numberOfCities != 10: 
+    while numberOfCities != 11: 
         pointList = generatePoints(numberOfCities,10)
         solution = bruteForce(pointList)
+        print(f"It took {solution.time} for {numberOfCities} points. Distance: {solution.distance} Solution: {solution.combination}\n")
+        numberOfCities = numberOfCities + 1
+
+
+    print('closestEdge')
+    #closest edge
+    numberOfCities = 3
+    while numberOfCities != 20: 
+        pointList = generatePoints(numberOfCities,10)
+        solution = closestEdge(pointList)
         print(f"It took {solution.time} for {numberOfCities} points. Distance: {solution.distance} Solution: {solution.combination}\n")
         numberOfCities = numberOfCities + 1
     #we'll run all 4 algorithms
